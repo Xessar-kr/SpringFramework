2e982dd1f4a33e7bad5ceed9a5ce82117d9365e0
Xessar <xessar.sakh@gmail.com>
2024-12-27 18:51:58 +0900

    Done task_2_1_5

--- src/main/java/feast/config/AppConfig.java
+++ src/main/java/feast/config/AppConfig.java
@@ -1,7 +1,6 @@
 package feast.config;
 
-import feast.models.Chicken3;
-import feast.models.Duck2;
+import feast.models.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +12,20 @@ public class AppConfig {
     @Bean
     public static Duck2 getDuck(Chicken3 chicken) {
         return new Duck2(chicken);
+    }
+
+    @Bean
+    public static Pig4 getPig(Lamb5 lamb5) {
+        return new Pig4(lamb5);
+    }
+
+    @Bean
+    public static Lamb5 getLamb(Goat6 goat) {
+        return new Lamb5(goat);
+    }
+
+    @Bean
+    static Horse7 getHorse(Camel8 camel) {
+        return new Horse7(camel);
     }
 }
